91b1e5a7be099c6ac6d4f3f487a6314dae2e4b6f
Gage Erwin <gageerwin@Gages-MacBook-Pro.local>
2023-10-10 20:41:50 -0500

    update

--- init.lua
+++ init.lua
@@ -17,11 +17,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require('custom.config')
 require('lazy').setup({
   -- Git related plugins
