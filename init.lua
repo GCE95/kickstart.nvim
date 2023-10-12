a1d661e0656963d5c12a688f606708c756353074
g0e002d <gage.erwin@walmart.com>
2023-10-11 21:01:29 -0500

    update

--- init.lua
+++ init.lua
@@ -56,7 +56,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',   opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -268,7 +268,8 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
+      'bash' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = true,
@@ -435,14 +436,6 @@ local servers = {
     },
   },
   pyright = {},
-  pylsp = {
-    pylsp = {
-      plugins = {
-        pyflakes = { enabled = false },
-        jedi_hover = { enabled = false },
-      },
-    },
-  },
   ruff_lsp = { ruff_lsp = { hoverProvider = false } },
   html = { filetypes = { 'html', 'twig', 'hbs' } },
 
--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -1,25 +1,27 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		event = "VeryLazy",
-		dependencies = { "mason.nvim" },
-		opts = function()
-			local nls = require("null-ls")
-			return {
-				root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-				sources = {
-					nls.builtins.formatting.fish_indent,
-					nls.builtins.diagnostics.fish,
-					nls.builtins.formatting.stylua,
-					nls.builtins.formatting.shfmt,
-					nls.builtins.formatting.prettierd,
-					nls.builtins.formatting.goimports,
-					nls.builtins.formatting.gofmt,
-				},
-			}
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-	},
+  {
+    'nvimtools/none-ls.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'mason.nvim' },
+    opts = function()
+      local nls = require 'null-ls'
+      return {
+        root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', 'Makefile', '.git'),
+        sources = {
+          nls.builtins.formatting.fish_indent,
+          nls.builtins.diagnostics.fish,
+          nls.builtins.formatting.stylua,
+          nls.builtins.formatting.shfmt,
+          nls.builtins.formatting.prettierd,
+          nls.builtins.formatting.goimports,
+          nls.builtins.formatting.gofmt,
+          nls.builtins.formatting.black,
+          nls.builtins.formatting.isort,
+        },
+      }
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+  },
 }
