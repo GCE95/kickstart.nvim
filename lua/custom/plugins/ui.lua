0f474e3d66ae15780de49b0f8b7176da19db4ec4
Gage Erwin <gage.erwin@live.com>
2024-05-24 13:01:09 -0500

    update

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -2,13 +2,18 @@ return {
 
 	-- colorscheme
 	{ 'navarasu/onedark.nvim' },
-	{ 'folke/tokyonight.nvim' },
+	{ 'sainnhe/gruvbox-material' },
 	{
 		'catppuccin/nvim',
 		priority = 1000,
 		config = function()
+			vim.g.gruvbox_material_background = 'hard'
+			vim.g.gruvbox_material_foreground = 'original'
+			vim.g.gruvbox_material_diagnostic_line_highlight = 1
+			vim.g.gruvbox_material_diagnostic_text_highlight = 1
+			vim.g.gruvbox_material_diagnostic_virtual_text = 'highlighted'
 			require('onedark').setup { style = 'dark' }
-			vim.cmd.colorscheme 'catppuccin'
+			vim.cmd.colorscheme 'gruvbox-material'
 		end,
 	},
 
